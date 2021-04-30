e89735686af1a1cc25a9423381a3b5b2142447d9
gao8319 <60917570+gao8319@users.noreply.github.com>
2021-04-30 09:52:52 +0800

    Add files via upload (#1)
    * Add files via upload
    
    This is the latest version on January 27, 2021, written by Gao.
    
    * Delete server_syj.cpp
    
    * Delete sock1.2.cpp
    
    * Add files via upload
    
    * Update server.cpp
    
    Format function name and codes.
    
    * Update server.cpp
    
    Add a space
    
    * Add files via upload
    
    Co-authored-by: syj2908 <56108144+syj2908@users.noreply.github.com>

--- server.cpp
+++ server.cpp
@@ -8,10 +8,10 @@
 #include <pthread.h>    //g++ -pthread -o server server.cpp
 #include <string.h>
 
-#define IP "192.168.140.128" //服务器IP
+#define IP "192.168.1.103" //服务器IP
 #define PORT 8000            //服务器端口号
 #define QUE_NUM 2           //最大连接数
-#define MAX_BUFF_LEN 1024    //最大缓冲区长度
+#define MAX_BUFF_LEN 1024  //最大缓冲区长度
 
 int conn_fd[QUE_NUM];   //所有用户的套接字
 using namespace std;
@@ -56,38 +56,115 @@ void ID_verify(int sock_fd)
 {
     char Key[100]={0};
     char passwd[] = "syj 123";
-
-    while(strcmp(Key,passwd))
+    char success[] = "login success, welcome!";
+    char fail[] = "login failed!";
+    while(1)
     {
         if (recv(sock_fd, Key, sizeof(Key), 0) > 0)
         {
-            format(Key,sizeof(Key));
-            cout << "User " << Key << " is trying to login." << endl;
+            if(strcmp(Key,passwd)!=0)
+            {
+                format(Key,sizeof(Key));
+                cout << "User " << Key << " is trying to login." << endl;
+                send(sock_fd, fail, (int)strlen(fail), 0);
+            }
+            else
+                break;
         }
     }
 
     cout << "User " << Key << " has login." << endl;
-    char success[] = "login success, welcome!";
-    send(sock_fd, success, sizeof(success), 0);
+    send(sock_fd, success, (int)strlen(success), 0);
 }
 
 void *send_func(void *arg)
 {
     INFO_SEND *info_send = (INFO_SEND *)arg;
 
-    if (send(info_send->dst_sock_fd, info_send->buffer, sizeof(info_send->buffer), 0) == -1)
+    if (send(info_send->dst_sock_fd, info_send->buffer, (int)strlen(info_send->buffer), 0) == -1)
     {
         cout << "send message fail" << endl;
         pthread_exit(0);
     }
     else
     {
-        cout << "MESSAGE SEND TO " << (info_send->NO == 0 ? 1 : 0) << ": " << info_send->buffer;
+        cout << "MESSAGE SEND TO " << (info_send->NO == 0 ? 1 : 0) << ": " << info_send->buffer<<endl;
     }
 
     pthread_exit(0);
 }
-
+void ftp_offline(int sock_fd)
+{
+    char filename[100] = "test.mp4";  //文件名
+    FILE *fp = fopen(filename, "wb"); //以二进制方式打开（创建）文件
+    char buffer[MAX_BUFF_LEN] = {0};  //文件缓冲区
+    int nCount;
+    cout<<"start1"<<endl;
+    while(1)
+    {
+        recv(sock_fd, buffer, MAX_BUFF_LEN, 0);
+        cout<<buffer<<endl;
+        if(strncmp(buffer, "ftpstart", 8) == 0)
+        {
+            cout<<"strat2"<<endl;
+            break;
+        }
+        
+    }
+    cout<<"start3"<<endl;
+    while(1)
+    {
+        nCount = recv(sock_fd, buffer, MAX_BUFF_LEN, 0);
+        //cout<<nCount<<endl;
+        if(strncmp(buffer, "ftpfin", 6) == 0)
+        {
+            cout<<"传输完毕"<<endl;
+            break;
+        }
+            
+        if(nCount>0)
+        {
+             fwrite(buffer,nCount,1,fp);
+        }
+    }
+    fclose(fp);
+    char success[]="File transfer success!";
+    cout<<"File transfer success!"<<endl;
+    send(sock_fd, success, sizeof(success), 0);
+}
+void ftp_online(int FTP_SEND)
+{
+    cout<<"FTP_SEND"<<"套接字"<<endl;
+    int FTP_RECV=1-FTP_SEND;
+    char ftpreq[] = "ftpreq";
+    send(conn_fd[FTP_RECV], ftpreq, (int)strlen(ftpreq), 0);
+    char ftpstart[] = "ftpstart";
+    char ftp_buffer[MAX_BUFF_LEN];
+    while(1)
+    {
+        recv(conn_fd[FTP_SEND], ftp_buffer, MAX_BUFF_LEN, 0);
+        if(strncmp(ftp_buffer, "ftpstart", 8) == 0)
+            break;
+    }
+    int test;
+    test=send(conn_fd[FTP_RECV], ftpstart, (int)strlen(ftpstart), 0);
+    cout<<test<<"开始"<<endl;
+    while(1)
+    {
+        test=recv(conn_fd[FTP_SEND], ftp_buffer, MAX_BUFF_LEN, 0);
+        cout<<"收"<<test<<endl;
+        if(strncmp(ftp_buffer, "ftpfin", 6) == 0)
+            break;
+        test=send(conn_fd[FTP_RECV], ftp_buffer, test, 0);
+        cout<<"发"<<test<<endl;
+    }
+    char success[] = "File transfer success!";
+    send(conn_fd[FTP_SEND], success, (int)strlen(success), 0);
+    char fin[] = "ftpfin";
+    _sleep(5*100);
+    send(conn_fd[FTP_RECV], fin, (int)strlen(fin), 0);
+    cout << "File transfer success!"<<endl;
+}
 void *recv_func(void *arg)
 {
     INFO *info = (INFO *)arg;
@@ -100,10 +177,24 @@ void *recv_func(void *arg)
 
     while (1)
     {
+
         if (recv(info->sock_fd, recv_buffer, sizeof(recv_buffer), 0) > 0)
         {
-            cout << "MESSAGE RECV FROM NO." << info->NO << ": " << recv_buffer;
+            cout << "MESSAGE RECV FROM NO." << info->NO << ": " << recv_buffer<<endl;
 
+            if(strncmp(recv_buffer, "ftpreqlixian", 12) == 0)
+            {
+                cout<<"recereq"<<endl;
+                ftp_offline(info->sock_fd);
+                continue;
+            }
+            if(strncmp(recv_buffer, "ftpreqzaixian", 13) == 0)
+            {
+                ftp_online(info->NO);
+                continue;
+            }
+            if (strncmp(recv_buffer, "quit", 4) == 0)
+                break;
             info_send.NO = info->NO;
             info_send.dst_sock_fd = (info->NO == 0) ? conn_fd[1] : conn_fd[0];
             strcpy(info_send.buffer, recv_buffer);
@@ -125,8 +216,7 @@ void *recv_func(void *arg)
         {
             send_result = pthread_join(send_thread1, NULL);
         }
-        if (strncmp(recv_buffer, "quit", 4) == 0)
-            break;
+
     }
 }
 
